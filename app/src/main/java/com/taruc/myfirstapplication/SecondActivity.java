47c81434ed84cb9eb16a775e5c4435adcb2ddf41
laijcuc <perdanav6>
2018-05-22 12:10:44 +0800

    INIT COMMIT

---
+++ app/src/main/java/com/taruc/myfirstapplication/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.taruc.myfirstapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        //grap the intent
+        Intent intent = getIntent() ;
+               String msg = intent.getStringExtra( "key") ;
+               //create textview on the fly
+               TextView textView = new TextView( this);
+                textView.setText(msg) ;
+                setContentView(textView) ;
+    }
+}
